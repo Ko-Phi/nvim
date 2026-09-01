1373b7c47c393b411f031e6844cc652fa1d950d5
Ko-Phi <172218929+Ko-Phi@users.noreply.github.com>
2026-08-31 17:27:45 -0700

    Add surrounds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,19 @@
 -- luacheck: globals vim
 local keymap = vim.keymap
-keymap.set("i", "jk", "<ESC>")
+
 keymap.set("n", "x", '"_x"')
+keymap.set("n", "$", "g_")
 
 keymap.set("n", "<leader>+", "<C-a>")
 keymap.set("n", "<leader>-", "<C-x>")
 
 keymap.set("n", "<leader>e", ":Neotree toggle=true<CR>")
+
+keymap.set("x", '<leader>"', 'c""<Esc>P', { desc = 'Surround Selection in ""' })
+keymap.set("x", "<leader>'", "c''<Esc>P", { desc = "Surround Selection in ''" })
+keymap.set("x", "<leader>[", "c[]<Esc>P", { desc = "Surround Selection in []" })
+keymap.set("x", "<leader>(", "c()<Esc>P", { desc = "Surround Selection in ()" })
+keymap.set("x", "<leader>{", "c{}<Esc>P", { desc = "Surround Selection in {}" })
 
 keymap.set("n", "gl", function()
 	vim.diagnostic.open_float()
